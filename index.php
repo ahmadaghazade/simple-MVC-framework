7073141904afd712c68e37346e1451ec5ee6d2fa
ahmadaghazade <ahmadaghazade18@gmail.com>
2024-10-05 19:35:39 +0330

    refactor in index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 // Here the app requires the classes
 use App\App\Core\Router;
@@ -13,7 +13,7 @@ $router = new Router();
 
 
 
-require_once '../routes/web.php';
+require_once 'routes/web.php';
 
 
 function dd($item)
